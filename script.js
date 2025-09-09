39dc4d7657563f6047222bc3da5a269924ab3b01
Cadu714 <00001079611939SP@al.educacao.sp.gov.br>
2025-09-09 10:46:48 -0300

    a

--- script.js
+++ script.js
@@ -29,4 +29,7 @@ botaoDeAcessibilidade.addEventListener('click', function(){
     document.body.classList.toggle('alto-contraste');
   })
 
+  const botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-expanded') === 'true';
+    botaoDeAcessibilidade.setAttribute('aria-expanded', !botaoSelecionado);
+
 })
